cfef6e987199fc73a27ccf19ccb403a908510158
Rodrigo <rdg.hora1@gmail.com>
2025-09-10 09:58:03 -0300

    feat: adicionar suporte para obter salas de chat e atualizar configurações do TypeScript

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,6 +1,7 @@
 import { CreateRoomForm } from "@/components/chat-forms/create-room";
 import { EnterRoomForm } from "@/components/chat-forms/enter-room";
 import { ModeToggle } from "@/components/mode-toggle";
+import { GetRooms } from "@/components/rooms";
 
 export default function ChatPage() {
   return (
@@ -14,8 +15,9 @@ export default function ChatPage() {
           <strong className="text-green-500">sala</strong> existente
         </h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-6xl mx-auto p-8 content-center min-h-screen ">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-6xl mx-auto p-8 content-center min-h-screen ">
         <CreateRoomForm />
+        <GetRooms />
         <EnterRoomForm />
       </div>
     </>
